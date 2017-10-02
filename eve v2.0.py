aafb2ebc8e07687d6c9008da8b4e233f3f6ad39d
Abhishek Bhagwat <Abhishek.bhagwat1999@gmail.com>
2017-10-02 19:04:42 +0530

    Update eve v2.0.py

--- eve v2.0.py
+++ eve v2.0.py
@@ -1,206 +1,123 @@
-#This code is written by Abhishek Bhagwat. Please give credits to the author if this repository is cloned and used. Find this author on GitHub : github.com/Abhishekbhagwat
+import urllib
+import time, sys
+import telepot, facebook, requests
+from telepot.loop import MessageLoop
+from events_database import database
+from telepot import DelegatorBot
+from telepot.delegate import pave_event_space, per_chat_id, create_open
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
-#Eve : The Event Bot
-
-#This bot is built to scrape upcoming events data from facebook pages of clubs at NTU and display them accprding to the user's preference
-
-
-#This bot uses Facebook's Graph API to make a request and then displays the event 
-
-
-
-import facebook     #Facebook module is imported to access the GraphAPI to make API calls
-import requests     #requests module is imported to make requests to the server to send and recieve the data
-import sys
-
+with open('TOKEN.txt', 'r') as token_file:
+    TOKEN = token_file.read()
 
 #A user access token from Facebook Developer page : developers.facebook.com
-
-
-'''This access token expires every two hours for security reasons but this can be overcome by using an extended access token. The one used below is an extended token 
+'''This access token expires every two hours for security reasons but this can be overcome by using an extended access token. The one used below is an extended token
 and is valid till November 7th 2017 '''
-
-
 graph = facebook.GraphAPI(access_token="EAABpaWOJYT8BALCrZBXNm6ZAGYZBPiJ7qxpEnMYWK0XZB1I7AbHQ5tfmZBnqhHKg2kAtUk9CV0ianlZBnPwWxpgtJ5mIZB9Jb6u17dHvMDRA3H8yfqX5ON1LRpxo9ZAnR5Xv9lAepH2Ohx338ZBjiO47IqpXlV8lvkY1o9UzFVaDQWQZDZD", version = "2.7")
 
-
-#The following code is the welcome code and will introduce the bot and displays the preferences available to chose from
-
-print ("Hi!! I'm Eve, the event bot. I will show you the most happening events at NTU from your favorite clubs")
-
-print("Please selecct the category from which you would like to see events.")
-
-print("""
-                1.Technology
-                2.Culture
-                3.Music         
-                4.Dance
-                5.Sports
-                6.Miscellaneous """)
+# initializing states for event choice, not really used yet
+CHOOSE_EVENT_CATEGORY = 0
+CHOSEN_EVENT_CATEGORY = 1
 
 
-x = int(input("Please input your choice"))  #Asks the user for input 
+class EveBot(telepot.helper.ChatHandler):
+
+    def __init__(self, *args, **kwargs):
+        super(EveBot, self).__init__(include_callback_query=True, *args, **kwargs)
+        self.state = CHOOSE_EVENT_CATEGORY
+        self.event_category = None
+
+    def on_chat_message(self, msg):
+        global cat_bot
+        content_type, chat_type, chat_id = telepot.glance(msg)
+
+        # default response 
+        response = 'Hi!! I\'m Eve, the event bot. I will show you the most happening events at NTU from your favorite clubs'
+
+        # handle only messages with text content
+        if content_type == 'text':
+
+            # get message payload
+            msg_text = msg['text']
 
 
-print("Your choice is ",x) #User's choice is output
+            if (msg_text.startswith('/')):
 
+                # parse the command excluding the '/' (from TGIF)
+                command = msg_text[1:].lower()
 
+                # interpret the commands and provide response accordingly
+                if (self.state == CHOOSE_EVENT_CATEGORY and command == 'events'):
 
-while count!=0:
+                    # get available event categories
+                    event_categories = ('Technology', 'Culture', 'Music', 'Dance', 'Sports', 'Miscellaneous')
 
-        if x == 1:   #if the coice is 1 then the following block of address is executed
-                print("You have chosen technology as your preference")
-                print("The most happening events are")
- 
+                    # custom keyboard
+                    event_categories_keyboard = ReplyKeyboardMarkup(one_time_keyboard=True, keyboard=[
+                        [KeyboardButton(text=event_category)] for event_category in event_categories
+                    ] + [[KeyboardButton(text='Nevermind')]])
 
-                database_events_tech = ['TGIF Hacks','Web exploitation','iNTUition',] # This list consists of data which is populated by events which are usually held regularly. Only such events are populated due to quality control.
+                    response = 'Please selecct the category from which you would like to see events.'
+                    bot.sendMessage(chat_id, response, reply_markup=event_categories_keyboard)
+                    self.state == CHOSEN_EVENT_CATEGORY
+                    # this somehow stops the bot from sending the response twice
+                    return
 
+            elif (msg_text == ('Technology' or 'Culture' or 'Music' or 'Dance' or 'Sports' or 'Miscellaneous')):
+                response = ('Your choice was ' + msg_text)
+                bot.sendMessage(chat_id, response)
 
-                for i in range (len(database_events_tech)):                               # for loop used to parse over the elements of the list                        
-                events = graph.search(type='event',q=[database_events_tech])      #API call is made to search for events in the list database_events_tech
-
-
-                for event in events['data']:                                            #Result obtained is stored in a list                                           
-                        print(event['id'],event['name'],event['description'])           #attributes of the event 'id','name','description' are printed
-
-
-                process = input(("Do you want to quit ? (y/n)"))
-
-
-                if process == 'y':
-                        count=0
+                if (msg_text == 'Technology'):
+                    database_events = database['database_events_tech']
+                elif (msg_text == 'Culture'):
+                    database_events = database['database_events_culture']
+                elif (msg_text == 'Music'):
+                    database_events = database['database_events_music']
+                elif (msg_text == 'Dance'):
+                    database_events = database['database_events_dance']
+                elif (msg_text == 'Sports'):
+                    database_events = database['database_events_sports']
+                elif (msg_text == 'Miscellaneous'):
+                    database_events = database['database_events_misc']
                 else:
-                        count+=1
+                    # maybe implement something later on - but this should never be run since the outer if already limits the inputs
+                    pass
 
-        elif x == 2:
-                print("You have chosen culture as your preference")
-                print("The most happening events are")
-
-
-                database_events_cult = []
-
-
-                for i in range (len(database_events_cult)):
-                        events = graph.search(type='event',q=[database_events_cult])
-
+                for i in range (len(database_events)):                               # for loop used to parse over the elements of the list
+                    # API call is made to search for events in the list database_events_tech
+                    events = graph.search(type='event',q=[database_events])
 
                 for event in events['data']:
-                        print(event['id'],event['name'],event['description'])
+                    #Result obtained is stored in a list
+                    response = (event['name'] + event['description'])
+                    print ('event_id: ', event['id'])
+                    bot.sendMessage(chat_id, response)
+
+            else:
+                # response = 'Please choose an event category!'
+                pass
+
+        # send the response
+        bot.sendMessage(chat_id, response)
 
 
-                process = input(("Do you want to quit ? (y/n)"))
+    def on_callback_query(msg):
+        global cat_bot
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+
+        # answer callback query or else telegram will forever wait on this
+        bot.answerCallbackQuery(query_id)
 
 
-                if process == 'y':
-                        count=0
-                else:
-                        count+=1
+bot = DelegatorBot(TOKEN, [
+    pave_event_space()
+    (per_chat_id(), create_open, EveBot, timeout=100)
+])
+MessageLoop(bot).run_as_thread()
 
+# show in cmd that bot is running
+bot_name = bot.getMe()['first_name']
+print(bot_name + ' is running...')
 
-
-        elif x == 3:
-                print("You have chosen Music as your preference")
-                print("The most happening events are")
-
-
-
-                database_events_mus = []
-
-
-                for i in range (len(database_events_mus)):
-                        events = graph.search(type='event',q=[database_events_mus])
-
-
-                for event in events['data']:
-                        print(event['id'],event['name'],event['description'])
-
-
-                process = input(("Do you want to quit ? (y/n)"))
-
-
-                if process == 'y':
-                        count=0
-                else:
-                        count+=1
-
-
-
-        elif x == 4:
-                print("You have chosen Dance as your preference")
-                print("The most happening events are")
-
-
-                database_events_dan = []
-
-
-                for i in range (len(database_events_dan)):
-                        events = graph.search(type='event',q=[database_events_dan])
-
-
-
-                for event in events['data']:
-                        print(event['id'],event['name'],event['description'])
-
-
-                process = input(("Do you want to quit ? (y/n)"))
-
-
-                if process == 'y':
-                        count=0
-                else:
-                        count+=1
-
-
-
-        elif x == 5:
-                print("You have chosen sports as your preference")
-                print("The most happening events are")
-
-
-                database_events_spr = []
-
-
-                for i in range(len(database_events_spr)):
-                        events = graph.search(type='event',q=[database_events_spr])
-
-
-                for event in events['data']:
-                        print(event['id'],event['name'],event['description'])
-
-
-                process = input(("Do you want to quit ? (y/n)"))
-
-
-                if process == 'y':
-                        count=0
-                else:
-                        count+=1
-
-
-
-        elif x == 6:
-                print("You have chosen Miscellaneous events as your preference")
-                print("The most happening events are")
-
-
-                database_events_misc = []
-
-
-                for i in range(len(database_events_misc)):
-                        events = graph.search(type='event',q=[database_events_misc])
-
-
-
-                for event in events['data']:
-                        print(event['id'],event['name'],event['description'])
-                
-
-                process = input(("Do you want to quit ? (y/n)"))
-
-
-                if process == 'y':
-                        count=0
-                else:
-                        count+=1
-
-
+while True:
+    time.sleep(10)
