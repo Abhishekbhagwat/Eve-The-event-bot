eca659dd7059e7787c5f35ee1f3b0bfaafe6f676
Milkweedz <Milkweedz@hotmail.com>
2017-10-02 21:27:44 +0800

    Moved iNTUition to beginning of list

--- events_database.py
+++ events_database.py
@@ -2,7 +2,7 @@
 #  usually held regularly. Only such events are populated due to quality control.
 
 database = {
-    'database_events_tech' : ['TGIF Hacks','Web exploitation','iNTUition',],
+    'database_events_tech' : ['iNTUition','TGIF Hacks','Web exploitation',],
     'database_events_culture' : [],
     'database_events_music' : [],
     'database_events_dance' : [],
