1c41d0efaaff77adc01c11645c07745a16b28d5d
Abhishek Bhagwat <Abhishek.bhagwat1999@gmail.com>
2017-10-03 08:32:08 +0530

    Add files via upload
    v2.0

--- events_database.py
+++ events_database.py
@@ -2,10 +2,10 @@
 #  usually held regularly. Only such events are populated due to quality control.
 
 database = {
-    'database_events_tech' : ['TGIF Hacks','Web exploitation','iNTUition',],
+    'database_events_tech' : ['TGIF Hacks','iNTUition',''],
     'database_events_culture' : [],
     'database_events_music' : [],
     'database_events_dance' : [],
     'database_events_sports' : [],
-    'database_events_misc' : [],
+    'database_events_misc' : ['NTU Dorothy Cheung Debating Championship 2017','NTU Model United Nations 2017',''],
 }
