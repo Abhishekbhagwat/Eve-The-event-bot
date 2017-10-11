e1c13e444da9e1a02615f491a457333e8079aaab
Abhishek Bhagwat <Abhishek.bhagwat1999@gmail.com>
2017-10-11 20:38:35 +0800

    latest release of database

--- events_database.py
+++ events_database.py
@@ -2,10 +2,11 @@
 #  usually held regularly. Only such events are populated due to quality control.
 
 database = {
-    'database_events_tech' : ['TGIF Hacks','iNTUition',''],
-    'database_events_culture' : [],
-    'database_events_music' : [],
-    'database_events_dance' : [],
-    'database_events_sports' : [],
-    'database_events_misc' : ['NTU Dorothy Cheung Debating Championship 2017','NTU Model United Nations 2017',''],
-}
+    'database_events_tech' : ['INTUition 2017','TGIF Hacks','Malicious Android APK','C3 Games creation workshop'],
+    'database_events_culture' : ['NTU Chingay Contingent 2018','From Paints to Prints Paper Marbling Workshop','Confluence',],
+    'database_events_music' : ['音你而声 A song for u',],
+    'database_events_dance' : ['Inter hall Dance competition','Joint dance concert'],
+    'database_events_sports' : ['X CAMPUS 2017','NTU Table Tennis Open 2017','NTU Karate Self-Defence Workshop 2017','NTU Bowling Trios 2017','NTUGSC Cricket Championship 2017','Inter Hall Games','Surf N Sweat','National Vertical Marathon'],
+    'database_events_misc' : ['NTU Pro Ams','Free Entry! Open to Public! TEDxNTU 2017: Here’s to the Gamechangers','Bilstein Suspension Workshop',''],
+    
+    }
